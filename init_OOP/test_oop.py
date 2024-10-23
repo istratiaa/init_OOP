2898d23f4f1d69a60534484ec4e42e844f61e228
tolya_job <tolya_job@example.com>
2024-10-23 15:22:45 +0300

    inc (property)

--- init_OOP/test_oop.py
+++ init_OOP/test_oop.py
@@ -2,12 +2,21 @@
 # наследуется и видим дочерними классами
 # __(private) приватные атрибуты, не наследуется, можно обращаться через сеттеры, геттеры и делитеры,
 # а еще через (object._Class__attribute ---> Name Mangling)
+
+class Ineger:
+    '''дескриптор'''
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
 class Person:
     '''данные для хранения персоны'''
 
     def __init__(self, name, age):
         self.__name = name
-        self.__age = age
+        self.age = age
 
     def conclusion(self):
         print(f"I am {self.__name} and to me {self.__age}")
@@ -29,7 +38,7 @@ class Person:
 
 if __name__ == '__main__':
     per = Person("Igor", 30)
-    # per.age = 100
+    per.age = 100
     print(per.age, per.__dict__)
     per.conclusion()
     print(dir(per))
